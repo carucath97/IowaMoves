41646b65814a68094a15c29508c0cc9dbcf45255
carucath97 <47608019+carucath97@users.noreply.github.com>
2019-07-30 16:51:59 +0100

    Updated the file
    Managed to clean up the table so that it used more meaningful values, got rid of columns that weren't seen to really correlate to the price (in hindsight, should have use the individual correlation messages to do this rather than just manually doing it based on 'intuition'). Also wrote a knn.reg() 'calculation'. Haven't been able to do much meaningful work with the data yet however (in that the predictions/error rates are something that I haven't even reached yet). I am VERY behind on this. To be honest, I didn't really understand what I was doing at all yesterday and I understand now but it feels like the level of understanding I have now is what everyone else had yesterday and that I won't get this done.

--- initialisation.R
+++ initialisation.R
@@ -1,66 +1,16 @@
 #install.packages("ggplot2")
+install.packages("FNN")
 library(class)
 library(ggplot2)
+library(FNN)
 
 setwd("C:/Users/Admin/Documents/IowaMoves")
 
 RawIowa_test <- read.csv("test.csv", sep = ",")
 RawIowa_train <- read.csv("train.csv", sep = ",")
 
-#RawIowa_test$MSSubClass <- factor(RawIowa_test$MSSubClass, levels = c(20,
-#                                                                      30,
-#                                                                      40,
-#                                                                      45,
-#                                                                      50,
-#                                                                      60,
-#                                                                      70,
-#                                                                      75,
-#                                                                      80,
-#                                                                      85,
-#                                                                      90,
-#                                                                      120,
-#                                                                      150,
-#                                                                      160,
-#                                                                      180,
-#                                                                      190),
-#                                  labels = c("1-STORY 1946 & NEWER ALL STYLES",
-#                                             "1-STORY 1945 & OLDER",
-#                                             "1-STORY W/FINISHED ATTIC ALL AGES",
-#                                             "1-1/2 STORY - UNFINISHED ALL AGES",
-#                                             "1-1/2 STORY FINISHED ALL AGES",
-#                                             "2-STORY 1946 & NEWER",
-#                                             "2-STORY 1945 & OLDER",
-#                                             "2-1/2 STORY ALL AGES",
-#                                             "SPLIT OR MULTI-LEVEL",
-#                                             "SPLIT FOYER",
-#                                             "DUPLEX - ALL STYLES AND AGES",
-#                                             "1-STORY PUD (Planned Unit Development) - 1946 & NEWER",
-#                                             "1-1/2 STORY PUD - ALL AGES",
-#                                             "2-STORY PUD - 1946 & NEWER",
-#                                             "PUD - MULTILEVEL - INCL SPLIT LEV/FOYER",
-#                                             "2 FAMILY CONVERSION - ALL STYLES AND AGES"))
-
 #remove id
 Iowa_train_NoId <- RawIowa_train[,-1]
-
-
-
-#names(RawIowa_test$MSZoning) = c("Agriculture",
-#                                 "Commercial",
-#                                 "Floating Village Residential",
-#                                 "Industrial",
-#                                 "Residential High Density",
-#                                 "Residential Low Density",
-#                                 "Residential Low Density Park ",
-#                                 "Residential Medium Density")
-
-#names(RawIowa_test$LotShape) = c("Regular",
-#                                 "Slightly irregular",
-#                                 "Moderately Irregular",
-#                                 "Irregular")
-# need to remove various column headers to clean up the data
-
-#boxplot(iowa_train$MSSubClass ~ iowa_train$MSSubClass)
 
 #investigate columns with NA values
 na_records <- is.na(iowa_train$LotFrontage)
@@ -97,19 +47,168 @@ iowa_train_noGarage <- iowa_train_noGarage[,-c(50:51)]
 # due to so many NA values, get rid of PoolQC, Fence and MisFeatures
 iowa_train_noMisc <- iowa_train_noGarage[, -c(57:59)]
 
+# get rid of lot shape as not really something people consider for the price
+iowa_train_lessLot <- iowa_train_noMisc[, -6]
+
+# combine the bathroom areas into one
+iowa_train_lessLot$TotalBathrooms <- (iowa_train_lessLot$BsmtFullBath + iowa_train_lessLot$BsmtHalfBath + iowa_train_lessLot$FullBath + iowa_train_lessLot$HalfBath)
+
+# can get rid of other four bathroom columns now
+iowa_train_noBath <- iowa_train_lessLot[, -c(38:41)]
+
+# get rid of all columns between zoning(2) and utilities (7) as they aren't hugely used in pricing
+iowa_train_noLot <- iowa_train_noBath[, -c(3:6)]
+
+# get rid of columns between utilies and neighborhood
+iowa_train_noLot <- iowa_train_noLot[, -c(4:5)]
+
+# get rid of columns between house quality and material quality
+iowa_train_noLot <- iowa_train_noLot[, -c(11:16)]
+
+# just keep total basement area
+iowa_train_noBsmt <- iowa_train_noLot[, -c(14:16)]
+
+# kitchens above grade is redundant since kitchen quality is also column
+iowa_train_noKitch <- iowa_train_noBsmt[, -24]
+
+# get rid of criteria between garage area and sale condition as not very important
+iowa_train_noPorch <- iowa_train_noKitch[, -c(29:39)]
+
+# need to order factor some of the columns, then convert to ints/numerics, then normalise
+iowa_train_noPorch$MSSubClass <- factor(iowa_train_noPorch$MSSubClass, ordered = TRUE, levels = c(30,
+                                                                                                  20,
+                                                                                                  120,
+                                                                                                  150,
+                                                                                                  40,
+                                                                                                  45,
+                                                                                                  50,
+                                                                                                  160,
+                                                                                                  70,
+                                                                                                  60,
+                                                                                                  180,
+                                                                                                  75,
+                                                                                                  80,
+                                                                                                  85,
+                                                                                                  90,
+                                                                                                  190),
+                                        labels = c("1-STORY 1945 & OLDER",
+                                                   "1-STORY 1946 & NEWER ALL STYLES",
+                                                   "1-STORY PUD (Planned Unit Development) - 1946 & NEWER",
+                                                   "1-1/2 STORY PUD - ALL AGES",
+                                                   "1-STORY W/FINISHED ATTIC ALL AGES",
+                                                   "1-1/2 STORY - UNFINISHED ALL AGES",
+                                                   "1-1/2 STORY FINISHED ALL AGES",
+                                                   "2-STORY PUD - 1946 & NEWER",
+                                                   "2-STORY 1945 & OLDER",
+                                                   "2-STORY 1946 & NEWER",
+                                                   "PUD - MULTILEVEL - INCL SPLIT LEV/FOYER",
+                                                   "2-1/2 STORY ALL AGES",
+                                                   "SPLIT OR MULTI-LEVEL",
+                                                   "SPLIT FOYER",
+                                                   "DUPLEX - ALL STYLES AND AGES",
+                                                   "2 FAMILY CONVERSION - ALL STYLES AND AGES"
+                                        ))
+# order zones by cheapest to most expensive
+iowa_train_noPorch$MSZoning <- factor(iowa_train_noPorch$MSZoning, ordered = TRUE, levels = c("A", "I", "RH", "RM", "RL", "RP", "FV", "C"))
+
+# order utilities by cheapest to most expensive
+iowa_train_noPorch$Utilities <- factor(iowa_train_noPorch$Utilities, ordered = TRUE, levels = c("ELO", "NoSeWa", "NoSewr", "AllPub"))
+
+# get rid of neighborhood and condition columns, so much infor that's hard to really order
+iowa_train_noNghbr <- iowa_train_noPorch[, -c(4:6)]
+
+# order type of building
+iowa_train_noNghbr$BldgType <- factor(iowa_train_noPrice$BldgType, ordered = TRUE, levels = c("TwnhsI", "TwnhsE", "Duplx", "2FmCon", "1Fam"))
+
+# get rid of housestyle as it's redundant (says stories in SubClass)
+iowa_train_noStyle <- iowa_train_noNghbr[, -5]
+
+# order quality
+iowa_train_noStyle$OverallQual <- factor(iowa_train_noPrice$OverallQual, ordered = TRUE, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+# order condition
+iowa_train_noStyle$OverallCond <- factor(iowa_train_noPrice$OverallCond, ordered = TRUE, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+# get rid of building type as NA values have been found (also covered in SubClass)
+iowa_train_noBuilding <- iowa_train_noStyle[, -4]
+
+# get rid of other quality factors as overall is already listed
+iowa_train_lessQual <- iowa_train_noBuilding[, -c(6:7)]
+
+# order foundation
+iowa_train_lessQual$Foundation <- factor(iowa_train_lessQual$Foundation, ordered = TRUE, levels = c("Wood", "Stone", "Slab", "PConc", "CBlock", "BrkTil"))
+
+# get rid of heating as doesn't necessarily correlate with price (cheapest and most expensive tended towards GasA)
+iowa_train_lessHeat <- iowa_train_lessQual[, -c(8:9)]
+
+# order air con
+iowa_train_lessHeat$CentralAir <- factor(iowa_train_lessHeat$CentralAir, ordered = TRUE, levels = c("N", "Y"))
+
+# order electric
+iowa_train_lessHeat$Electrical <- factor(iowa_train_lessHeat$Electrical, ordered = TRUE, levels = c("SBrkr", "FuseA", "FuseF", "FuseP", "Mix"))
+
+# get rid of kitchen quality
+iowa_train_lessHeat <- iowa_train_lessHeat[, -15]
+
+# order functionality
+iowa_train_lessHeat$Functional <- factor(iowa_train_lessHeat$Functional, ordered = TRUE, levels = c("Sal", "Sev", "Maj2", "Maj1", "Mod", "Min2", "Min1", "Typ"))
+
+# order sale condition
+iowa_train_lessHeat$SaleCondition <- factor(iowa_train_lessHeat$SaleCondition, ordered = TRUE, levels = c("Partial", "Family", "Alloca", "AdjLand", "Abnorml", "Normal"))
+
+# get rid of zoning as it has NA values (and not sure what to change it to)
+iowa_train_noZone <- iowa_train_lessHeat[, -2]
+
+# make a new table to store the numerics
+iowa_train_numeric <- iowa_train_noZone
+
+# make the factors into numerics
+iowa_train_numeric$MSSubClass <- as.numeric(iowa_train_numeric$MSSubClass)
+iowa_train_numeric$Utilities <- as.numeric(iowa_train_numeric$Utilities)
+iowa_train_numeric$OverallQual <- as.numeric(iowa_train_numeric$OverallQual)
+iowa_train_numeric$OverallCond <- as.numeric(iowa_train_numeric$OverallCond)
+iowa_train_numeric$Foundation <- as.numeric(iowa_train_numeric$Foundation)
+iowa_train_numeric$CentralAir <- as.numeric(iowa_train_numeric$CentralAir)
+iowa_train_numeric$Electrical <- as.numeric(iowa_train_numeric$Electrical)
+iowa_train_numeric$Functional <- as.numeric(iowa_train_numeric$Functional)
+iowa_train_numeric$SaleCondition <- as.numeric(iowa_train_numeric$SaleCondition)
 
 #for now, just see the rest as a data frame
-dataset <- as.data.frame(lapply(iowa_train_lessBsmt, as.integer))
+dataset <- as.data.frame(lapply(iowa_train_lessLot, as.integer))
 
 # need a feature scaling function
-#scaling <- function(x) { ((x - min(x)) / (max(x) - min(x))) }
+scaling <- function(x) { ((x - min(x)) / (max(x) - min(x))) }
 
-#iowa_normalised <- as.data.frame(lapply(Iowa_train_NoId, scaling))
+iowa_normalised[, -19] <- as.data.frame(lapply(iowa_train_numeric[, -19], scaling))
 
+# split the date into training and testing -- doing a 60:40 split
+iowa_training <- iowa_normalised[1:876,-19]
+
+iowa_testing <- iowa_normalised[877:1460,-19]
+
+iowa_training_true <- iowa_normalised[1:876,19]
+
+k_value <- floor(sqrt(nrow(iowa_training)))
+
+# use knn.reg (reg=regression)
+iowa_predictions <- knn.reg(iowa_training, iowa_testing, iowa_training_true, k=k_value)
+
+iowa_reference <- iowa_train_noZone[877:1460, 1]
+
+# table results
+prediction_table <- table(iowa_predictions, iowa_reference)
+  
 correlationMatrix <- cor(dataset[,1:8])
 correlationMatrix2 <- cor(dataset[,8:16])
 correlationMatrix3 <- cor(dataset[,16:25])
 correlationMatrix4 <- cor(dataset[,25:40])
-correlationMatrix5 <- cor(dataset[,41:55])
-correlationMatrix6 <- cor(dataset[,56:70])
-correlationMatrix7 <- cor(dataset[,65:72])
+correlationMatrix5 <- cor(dataset[,41:50])
+correlationMatrix6 <- cor(dataset[,51:61])
+#correlationMatrix7 <- cor(dataset[,65:72])
+
+# make a new dataset
+iowa_data <- as.data.frame(lapply(iowa_train_noZone, as.integer))
+
+# compare the correlation of certain factors with the sales price??
+cor(iowa_data$MSSubClass, iowa_data$SalePrice) # -0.08428414
+cor(iowa_data$OverallQual, iowa_data$SalePrice) # 0.7909816
